b1e9f7459a8cd2bdad6bf8d52e08413bf9d7d13e
Archit Tiwary <tiwaryart007@gmail.com>
2026-08-05 12:26:14 +0000

    commited

---
+++ basics/variable_1.js
@@ -0,0 +1,12 @@
+const accountId=2332344
+let accountEmail="akki@google.com"
+var accountPassword="12344"
+accountCity="Jamshedpur"
+
+accountEmail="ak@gmail.com"
+accountPassword="45454"
+accountCity="Bengaluru"
+
+console.log(accountId)
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
