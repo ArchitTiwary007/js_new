8cbcd5544e25e4ef01bb0965587a154ddacc597b
Archit Tiwary <tiwaryart007@gmail.com>
2026-08-07 03:00:10 +0000

    conversion of operations

---
+++ basics/03_conversionoperation.js
@@ -0,0 +1,26 @@
+let score="11ac"
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNUmber=Number(score)
+// console.log(typeof valueInNUmber);
+// console.log(valueInNUmber);
+
+//conversion
+//"33"=>33
+//"33"=>NaN
+//true=>1;false=>0
+
+let isLoggedIn="tappu"
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+//""=>false
+//"tappu"=>true
+
+let someNum=11
+
+let strNum=String(someNum)//conversion to string
+console.log(strNum);
+console.log(typeof strNum);
+
