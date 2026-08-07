17899f4c252634c18ab1e0bbdd62c8b3d73cf1fd
Archit Tiwary <tiwaryart007@gmail.com>
2026-08-07 02:39:21 +0000

    commit

---
+++ basics/datatype_2.js
@@ -0,0 +1,22 @@
+"use strict";//teats as newer version
+
+// alert("hello")
+//alert(3+3)//we are using node js not browser=>alert gives error here
+//high code readability
+
+let name="tappu"
+let age="21"
+let place=null;
+
+//number
+//bigint
+//string
+//null=>standalone value
+//undefined=>
+//symbol=>unique
+
+//object
+
+console.log(typeof "tappu")
+console.log(typeof null)//output=>object
+console.log(typeof undefined)//output=>undefined
