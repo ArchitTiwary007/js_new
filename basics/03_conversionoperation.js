4d509bc61e26ca199bdc30ea7f22e8bbecabea97
Archit Tiwary <tiwaryart007@gmail.com>
2026-08-07 03:14:30 +0000

    modified

--- basics/03_conversionoperation.js
+++ basics/03_conversionoperation.js
@@ -1,3 +1,5 @@
+//tc39 && mdn for resources!!
+
 let score="11ac"
 // console.log(typeof score);
 // console.log(typeof (score));
@@ -21,6 +23,35 @@ let booleanIsLoggedIn=Boolean(isLoggedIn)
 let someNum=11
 
 let strNum=String(someNum)//conversion to string
-console.log(strNum);
-console.log(typeof strNum);
+// console.log(strNum);
+// console.log(typeof strNum);
 
+//OPERATIONS//
+let value=5
+let negval=-value
+// console.log(negval);
+
+let str1="hello"
+let str2=" sir"
+
+let str3=str1+str2
+// console.log(str3);
+
+//problems
+// console.log("1"+2)
+// console.log("2"+1)
+// console.log("1"+2+2);//starting val pe depend krta hai
+// console.log(1+2+"2");//This works(But not used in big projects)
+
+// console.log((3+4)*5%3);
+
+// console.log(+true);//true+ not possi
+// console.log(+"")
+
+let n1,n2,n3//Not a good practice
+n1=n2=n3=2+2
+
+let gameCounter=111
+// gameCounter++;
+++gameCounter;
+console.log(gameCounter);
