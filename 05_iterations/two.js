14515dce6e59fcb78cc7920411248f03548247f5
Archit Tiwary <tiwaryart007@gmail.com>
2026-08-14 03:57:10 +0000

    new commits

---
+++ 05_iterations/two.js
@@ -0,0 +1,24 @@
+//while
+
+let index=0
+while (index<=10) {
+    // console.log(`Value of index is ${index}`);
+    index=index+2;//even numbers print
+}
+
+let myArr=["batman","ironman","spiderman"]
+let i=0
+while(i<myArr.length){
+    // console.log(`Value is ${myArr[i]}`);
+    i=i+1;//i++ 
+}
+
+//do-while
+
+// let score=1
+// minimum ek baar chalega loop!!
+let score=11
+do {
+    console.log(`Score is ${score}`);
+    score++;
+} while (score<=10);
